8db17790a103b85bf93c26c74dd188a7aff8b8e7
Victor Ohashi <victor.ohashi@govoll.com>
2024-11-23 13:13:11 -0300

    docs: Fix typo

--- packages/teil_forms/lib/src/utils/controlled_text_field.dart
+++ packages/teil_forms/lib/src/utils/controlled_text_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:teil_forms/src/form/form.dart';
 
-/// A mixin that provides a [TextEditingController] for a [BaseFormField] of type [String].
+/// Provides [TextEditingController] for a [BaseFormField] of type [String].
 ///
 /// - Can be used with [TextField] or relatives that use [TextEditingController] to control the text.
 mixin ControlledTextField on BaseFormField<String?> {
